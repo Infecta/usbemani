7fa786bb5327aab610d366b9a8280b5ffa6128c6
Infecta <infectapvp@gmail.com>
2025-09-16 03:01:11 +0800

    Dockerfile edits

--- controllers/lain/mini-4/rev1/config.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#define CONTROLLER_RGB_LEDS_TURNTABLE 12
-#define CONTROLLER_RGB_LEDS_PER_KEY   3
-
-#include "../config.h"
-
-/*** Chain Inclusion ***/
-#include_next "config.h"
--- layouts/default/lain/mini-4/rules.mk
+++
@@ -1,2 +0,0 @@
-TARGET_BOARD      ?= lain/rainbow-flyer/rev1
-TARGET_CONTROLLER ?= lain/mini-4/rev2
